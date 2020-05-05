3e2ee5a7d59ac39ce2c085c7c56b3e4c352f7908
Anas Abdussalam Lawan <64676236+AnasMaimalee@users.noreply.github.com>
2020-05-05 18:23:08 +0100

    Update connect-create-table.php
    Updated corrected by I A senator

--- connect-create-table.php
+++ connect-create-table.php
@@ -2,11 +2,11 @@
 $servername ="localhost";
 $username = "root";
 $password = "";
-$dbname = "mydb"
+$dbname = "mydb";
 
 //create connection
 // the connection is not working and i dont know why pls help me fixed it.
-$conn = new mysqli("$servername", "$username", "$password", $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 //check connection
 
@@ -21,9 +21,9 @@ $sql = "CREATE TABLE myDetails (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_STAMP
-)",
+    email VARCHAR(50)
+    
+)";
 
 if($conn->query($sql)==TRUE){
     echo" Table created successfully";
